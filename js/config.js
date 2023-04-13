9f901b51ec4e958b8657bb5b62419c685a5f737e
Miguel Velasquez <veruiz.miguel@gmail.com>
2023-04-12 19:08:06 -0500

    new config

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+  cognito: {
+    userPoolId: "us-east-1_7d3sMHowu", // e.g. us-east-2_uXboG5pAb
+    userPoolClientId: "65caj4u7e7ipmkqe2ig6lv5erg", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+    region: "us-east-1", // e.g. us-east-2
+  },
+  api: {
+    invokeUrl: "", // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+  },
+};
